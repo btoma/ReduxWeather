dba39e0e4b86145734bab1d226813236dae266a0
Biven <biven@lougeek.com>
2018-09-11 08:15:15 -0400

    Create new component and added new package to node sparklines and added switch checker for reducer weather.

--- src/actions/index.js
+++ src/actions/index.js
@@ -2,14 +2,14 @@ import axios from 'axios';
 
 const API_KEY = '242a31beb4615140fd945c4fa8188a4a';
 
-const ROOT_URL = `https://samples.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
+const ROOT_URL = `https://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
 
 export const FETCH_WEATHER = 'FETCH_WEATHER';
 
 
 export  function fetchWeather(city) {
     const url = `${ROOT_URL}&q=${city},us`;
-    const request = axios.get(url);
+    const request = axios.get(url); // behave like jquery ajax request reach out to url in form of get and return promise.
     return {
         type: FETCH_WEATHER,
         payload: request
